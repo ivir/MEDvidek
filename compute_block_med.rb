07408ada8aedc8429d274b0a0ffceb6d66b71576
Jan Mareš <jan.mares@manast.eu>
2015-07-06 07:07:04 +0000

    zakladni definice vypocetniho bloku

--- compute_block_med.rb
+++ compute_block_med.rb
@@ -1,2 +1,21 @@
 class ComputeBlockMED
+
+    public
+        #nastaveni datasetu s nimz se bude operovat
+        def set_dataset(sdataset)
+            
+        end
+        
+        #compute_block urcuje co se bude pocitat
+        def compute_block(cblock)
+            
+        end
+        
+        #spusti provedeni vypocetniho bloku nad datasetem
+        def compute()
+        end
+        
+        #navrati upraveny dataset
+        def get_dataset()
+        end
 end
