73f4b633f06c22487c21e4c2ee70f03048eafa96
Jan Mareš <jan.mares@manast.eu>
2016-01-10 09:43:24 +0100

    Vytvořena agregační fce pro výpočet hodnot nad datasetem.

--- modules/compute.rb
+++ modules/compute.rb
@@ -5,57 +5,96 @@ require_relative('../libs/dentaku/lib/dentaku')
 class Compute < ModuleMED
 
   def properties(memory,fdata)
-    printf "\nSpusten Compute\n"
+    #printf "\nSpusten Compute\n"
     #print fdata
     #print memory
     @what = memory[fdata["source"]]
     @source = fdata["source"]
     @destination = fdata["store"]
+    @store = memory[@destination]
     @calculate = fdata["calculate"]
     @calculator = Dentaku::Calculator.new
 
     memory.each_key { |key,value|
       @calculator.store(key,value) if value.is_a?(Integer) || value.is_a?(Float)
     }
+    @calculator.store("SIZE",@source.length)
   end
 
   #compute rozseka retezec pomoci operatoru (+-*/%) a nasledne promenne nahradi za variantu pro pristup do datasetu pro provedeni dane operace.
   def compute(fdata)
-    print "Jdu spocitat: " + @calculate.to_s + "\n"
+    #print "Jdu spocitat: " + @calculate.to_s + "\n"
     @calculator.evaluate(@calculate)
   end
   
   def execute(fdata)
-    printf "Jdu pracovat\n"
+    #printf "Jdu pracovat\n"
     temp = Hash.new
     @what.each do |value|
-      print value
+      #print value
       value.each_pair { |key,val|
-        print "K: #{key} V: #{val} \n"
+        #print "K: #{key} V: #{val} \n"
         @calculator.store(key,val)
       }
+
       temp.store(@destination,compute(nil))
+
       value.merge!(temp)
+
     end
 
-    print @what.to_s
+
   end
 
 end
 
 #Agregate provede agregaci celeho sloupce v predanem datasetu
-class Agregate < ModuleMED
+class Agregate < Compute
 
   def properties(memory,fdata)
-    printf "Spusten Agregate\n"
-    print fdata
-    @what = memory[fdata["source"]]
-    @source = fdata["source"]
-    @destination = fdata["store"]
+    @operation = fdata["operation"]
+    super(memory,fdata)
   end
 
   def execute(fdata)
     printf "Jdu pracovat Agregate\n"
+    first = true
+    @what.each do |value|
+      #print value
+      value.each_pair { |key,val|
+        #print "K: #{key} V: #{val} \n"
+        @calculator.store(key,val)
+      }
+      unless first
+        @store = cumul(@operation,@store,compute(nil))
+      else
+        @store = compute(nil)
+        first = false
+      end
+
+      print @store
+      print "\n"
+
+    end
+
+  end
+
+  def cumul(oper,value_a,value_b)
+    out = case oper
+            when "+"
+              value_a + value_b
+            when "-"
+              value_a - value_b
+            when "*"
+              value_a * value_b
+            when "/"
+              value_a / value_b
+            when "%"
+              value_a % value_b
+            else
+              eval("value_a " + oper + " value_b")
+          end
+    out
   end
 
 end
