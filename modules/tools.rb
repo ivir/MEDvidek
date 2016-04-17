f79032bcf0a233a10c2d1201f4fe11bf421ed408
Jan Mareš <jan.mares@manast.eu>
2016-04-17 03:55:21 +0200

    Zjednodušení kódu.

--- modules/tools.rb
+++ modules/tools.rb
@@ -27,3 +27,24 @@ class Ping < ModuleMED
   end
 
 end
+
+module Format
+  def convert(data)
+      data.gsub!(/^".*"$/,'')
+
+      if data =~ /^[+-]?\d+\s*$/
+        return (Integer(data));
+      end
+      if data =~ /^[+-]?\d+\s*\d*\.\d+[e+\-\d]*\s*$/
+        return (Float(data));
+      end
+      if data =~ /^[+-]?\d+[\S\s]*\d*,?\d+[e+\-\d]*\s*$/
+        #pouzita ceska forma zapisu
+        data.gsub!(/[^+\-0-9,e]/,'')
+        data.tr!(',','.')
+        return (Float(data))
+      end
+      #nebylo rozpoznano co to jest -> ulozime jak to je
+      String(data)
+  end
+end
