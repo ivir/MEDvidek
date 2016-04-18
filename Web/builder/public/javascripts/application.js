96d04fc55b225a9204a22cca5d5685f6402153a4
Jan Mareš <jan.mares@manast.eu>
2016-04-18 23:52:30 +0200

    Podpora vytváření receptu + příprava generování

--- Web/builder/public/javascripts/application.js
+++ Web/builder/public/javascripts/application.js
@@ -1,1 +1,33 @@
 // Put your application scripts here
+document.addEventListener("deviceready",pripraven);
+$(document).ready(function () { pripraven(); });
+
+function pripraven(){
+    
+}
+
+function loadModule(file){
+    var formular,fsource;
+    fsource = "/modules/" + file + ".html";
+    $.get(fsource, function( data ) {
+        var sr = $(data)
+        sr.appendTo($(".recipe"));
+        return false;
+    });
+    return false;
+}
+
+function createRecipe(){
+    var instrukce = $(".recipe");
+    var moduly = instrukce.find("div");
+    var recept = "";
+    $.each(moduly,function (index, modul){
+        recept += modul.className +":\n";
+        alert(recept);
+        var parametry = $(modul).find("input");
+        $.each(parametry, function (index,parametr){
+            recept += "\t" + parametr.name + ":" + parametr.value + "\n";
+        });
+    });
+    alert(recept);
+}
