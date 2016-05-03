521d8f04bc5f0e6b700a4a96dac5d3b2eafd6fd6
Jan Mareš <jan.mares@manast.eu>
2016-05-03 09:14:01 +0200

    Přidán základ generování reportu a souboru o závislostech

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# Zavislosti
+gem 'dentaku'
+gem 'nokogiri'
+
+#arbitr
+gem 'yaml'
+
+#LoadMail
+gem 'net/mail'
+
+#Report
+gem 'pdfkit'
+gem 'wkhtmltopdf'
---
+++ modules/report.rb
@@ -0,0 +1,57 @@
+require_relative('../module_med')
+require 'erb'
+require 'ostruct'
+
+#-----------
+# zdroj - http://stackoverflow.com/questions/3242470/problem-using-openstruct-with-erb/8293786#8293786
+# trida pro omezeni pristupu k datum
+
+class ErbBinding < OpenStruct
+  def get_binding
+    return binding()
+  end
+end
+
+#-----------
+class Report < ModuleMED
+
+  def properties(memory,fdata)
+    #printf "Spusten\n"
+    #print fdata
+    @memory = memory
+    @template = fdata["template"]
+    @orientation = fdata["orientation"]
+    @format = fdata["format"]
+    @store = fdata["store"]
+    @file = fdata["file"]
+
+    @memory["output"]
+    ''
+  end
+
+  def execute(fdata)
+    #printf "Jdu pracovat\n"
+
+    vars = ErbBinding.new(@memory)
+
+    template = "foo <%= bar %>"
+    erb = ERB.new(File.read(@template))
+
+    vars_binding = vars.send(:get_binding)
+    vysledek = erb.result(vars_binding)
+
+    case @format
+      when "pdf"
+        # generujeme do PDF
+      when "txt"
+        # generovani do TXT souboru
+      when "html"
+        # generovani do HTML
+      else
+        #neznamy format -> ulozime do store
+        @memory[@store] = vysledek unless @store.nil?
+    end
+
+  end
+
+end
