f8c1ca3b668d62d9f04c3685ad8cc4932b17995c
Jan Mareš <jan.mares@manast.eu>
2016-06-17 09:51:11 +0200

    Zakomentován původní třída DBStorage

--- db_storage.rb
+++ db_storage.rb
@@ -13,8 +13,8 @@
 #
 #
 #
-gem 'sqlite3', '~> 1.3.10'
-require 'sqlite3'
+#gem 'sqlite3', '~> 1.3.10'
+#require 'sqlite3'
 
 class DB_row
    def initialize
@@ -68,7 +68,7 @@ class Dataset
         end
 
 
-    def delete_if
+        def delete_if
             if block_given?
                 @data.delete_if{|i| yield(i)}
             end
@@ -214,89 +214,89 @@ class Dataset
     end
 end
 
-class DBStorage
-private
-    
-    
-    
-public
-    #construktor fce
-    def initialize
-        #@data = surova data; @changes = pole zmen
-        @data = SQLite3::Database.new(":memory:")
-        @changes = Hash.new
-    end
+#class DBStorage
+#private
 
-    def create
-        return Dataset.new
-    end
 
-    #prida sloupec
-    #what = jakou tabulku rozsiruje
-    #column = nazev rozsirujici polozky
-    def column_add(what, column)
-        ctype = "TEXT"
-        ctype = "INT" if(column.is_a?(Integer))
-        ctype = "FLOAT" if(column.is_a?(Float))
 
-        query = @data.prepare "Create table :name (Id INTEGER PRIMARY KEY, :column :ctype)"
-        query.stm (what + "_" + column.__id__), column.__id__, ctype
-        query.execute
+#public
+#    #construktor fce
+#    def initialize
+#        #@data = surova data; @changes = pole zmen
+#        @data = SQLite3::Database.new(":memory:")
+#        @changes = Hash.new
+#    end
 
-    end
-    
-    #prejmenuje sloupce z from na to
-    # chybi zpusob vyuziti krom prejmenovavani na vystupu
-    def column_rename(from, to)
-        
-    end
-    
-    
+#    def create
+#        return Dataset.new
+#    end
+
+ #   #prida sloupec
+ #   #what = jakou tabulku rozsiruje
+ #   #column = nazev rozsirujici polozky
+ #   def column_add(what, column)
+ #       ctype = "TEXT"
+ #       ctype = "INT" if(column.is_a?(Integer))
+ #       ctype = "FLOAT" if(column.is_a?(Float))
+
+#        query = @data.prepare "Create table :name (Id INTEGER PRIMARY KEY, :column :ctype)"
+#        query.stm (what + "_" + column.__id__), column.__id__, ctype
+#        query.execute
+#
+#    end
+
+ #   #prejmenuje sloupce z from na to
+ #   # chybi zpusob vyuziti krom prejmenovavani na vystupu
+ #   def column_rename(from, to)
+
+ #   end
+
+
     #odstrani dany sloupec
-    def column_remove(what)
-        
-    end
-    
+ #   def column_remove(what)
+
+ #   end
+
     #prida radek
     #where - do jake tabulky
     #sdata - data do konkretnich sloupcu
     # funguje na principu zapsani zmen do @changes, pricemz tyto zmeny se pomalu zapisuji do originalni databaze,
     # aby v situaci radku, ktery se bude casto menit se menit v pameti a do DBStorage se ulozil az po ustabilizovani
-    def add(where,sdata)
-        @changes[where] = sdata
-    end
-    
+ #   def add(where,sdata)
+ #       @changes[where] = sdata
+ #   end
+
     #odstrani radek
-    def remove(what)
-        @changes[what] = nil
-    end
-    
+ #   def remove(what)
+ #       @changes[what] = nil
+ #   end
+
     #aktualizace radku
-    def update(how, condition)
-    end
-    
+ #   def update(how, condition)
+ #   end
+
     #nacteni dat
-    def load(what)
-        @data.load(what) if(what.class == String)
-        @data = what.clone() if(what.class == SQLite3::Database)
-    end
-    
+ #   def load(what)
+ #       @data.load(what) if(what.class == String)
+ #       @data = what.clone() if(what.class == SQLite3::Database)
+ #   end
+
     #ulozeni dat
-    def store(where)
-        tmp = SQLite3::Database.new(where)
-        tmp = @data.clone
-        tmp.close
-    end
-    
-    #ulozeni vsech zmen
-    def flush
-    end
-    
-    #smazani vsech zmen
-    def clear
-    end
-    
+ #   def store(where)
+ #       tmp = SQLite3::Database.new(where)
+ #       tmp = @data.clone
+ #       tmp.close
+ #   end
+
+ #   #ulozeni vsech zmen
+ #   def flush
+ #   end
+
+ #   #smazani vsech zmen
+ #   def clear
+ #   end
+
     #konfigurace pracovniho prostredi
-    def configuration(parameters)
-    end
-end
+ #   def configuration(parameters)
+ #   end
+#end
