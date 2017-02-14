757aa5f4cf44152a66a4d4732ec7d2ae583f484e
Jan Mareš <jan.mares@manast.eu>
2017-02-14 19:56:35 +0100

    Refactoring pro využití std. knihovny CSV

--- modules/export_csv.rb
+++ modules/export_csv.rb
@@ -33,53 +33,62 @@ class ExportCSV < ModuleMED
     td = Array.new
     data = ""
 
-    ds = @memory[@store]
-    if(@columns.nil?)
-      header = separate(ds.columns)
-      data += header + "\n"
-      ds.each do |row|
-        data += separate(row.values) + "\n"
-      end
-    else
-      #resime prejmenovnai prvku, apod
-      arr = Array.new
-      @columns.each do |col|
-        unless (col.class == Hash)
-          arr.push(col)
-        else
-          col.each_pair do |key,value|
-            arr.push(value)
-          end
-        end
-      end
-      data += separate(arr) + "\n"
-      # nyni nahrajeme data jak je chteji
-      ds.each do |row|
-        arr.clear
-        @columns.each do |col|
-          unless col.class == Hash
-            if row[col].nil?
-              arr.push(format(row[col.downcase]))
-            else
-              arr.push(format(row[col]))
-            end
+    case @type
+      when 'csv'
+        separator = ','
+      when 'ssv'
+        separator = ';'
+      else
+        separator = ','
+    end
 
+    ds = @memory[@store]
+
+    CSV.open(@file,"w",col_sep: separator) { |rcsv|
+      if(@columns.nil?)
+        rcsv << ds.columns
+        ds.each do |row|
+          rcsv << row.values
+        end
+      else
+        #resime prejmenovnai prvku, apod
+        arr = Array.new
+        @columns.each do |col|
+          unless (col.class == Hash)
+            arr.push(col)
           else
             col.each_pair do |key,value|
-              if row[key].nil?
-                arr.push(format(row[key.downcase]))
+              arr.push(value)
+            end
+          end
+        end
+        rcsv << arr
+        # nyni nahrajeme data jak je chteji
+        ds.each do |row|
+          arr.clear
+          @columns.each do |col|
+            unless col.class == Hash
+              if row[col].nil?
+                arr.push(format(row[col.downcase]))
               else
-                arr.push(format(row[key]))
+                arr.push(format(row[col]))
+              end
+
+            else
+              col.each_pair do |key,value|
+                if row[key].nil?
+                  arr.push(format(row[key.downcase]))
+                else
+                  arr.push(format(row[key]))
+                end
               end
             end
           end
+          rcsv << arr
         end
-        data += separate(arr) + "\n"
       end
-    end
+    }
 
-
-    File.write(@file,data)
   end
 
   private
