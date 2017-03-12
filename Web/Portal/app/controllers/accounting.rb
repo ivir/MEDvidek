9c33dc6f268b062936d2892efb81ec5a4a0d790b
Jan Mareš <jan.mares@manast.eu>
2017-03-12 23:04:50 +0100

    zakladni kostra správy SIM a účtů

---
+++ Web/Portal/app/controllers/accounting.rb
@@ -0,0 +1,46 @@
+Portal::App.controllers :accounting do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :index do
+
+  end
+
+  get :list do
+
+  end
+
+  get :export do
+
+  end
+
+  post :create do
+
+  end
+
+  post :update do
+
+  end
+
+  post :delete do
+
+  end
+
+end
