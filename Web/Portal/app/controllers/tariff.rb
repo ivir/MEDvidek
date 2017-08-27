355cfe23ac1ee698e2ea28ac8dee434a17f09ae5
Jan Mareš <jan.mares@manast.eu>
2017-08-27 11:58:02 +0200

    Úprava kontroléru (Tariff) pro podporu manipulaci s tarifním modelem.

--- Web/Portal/app/controllers/tariff.rb
+++ Web/Portal/app/controllers/tariff.rb
@@ -2,7 +2,48 @@ Portal::App.controllers :tariff do
   layout "main"
 
   get :index do
+    @limits = Limit.all
     @tariffs = Tariff.all
+    @packages = Package.all
+
+    render "tariff/index"
+  end
+
+  post :create, :map =>"create/*model" do
+    model = params[:model]
+    case model
+      when "limit"
+        limit = Limit.new
+        limit.name = params[:name]
+        limit.parameter = params[:parameter]
+        limit.save
+      when "tariff"
+        tariff = Tariff.new
+        tariff.name = params[:name]
+        tariff.price = params[:price]
+        tariff.limit = params[:limit_id]
+        tariff.save
+      when "package"
+        package = Package.new
+        package.name = params[:name]
+        package.save
+    end
+    render "tariff/index"
+  end
+
+  post :delete, :map =>"delete/*model" do
+    model = params[:model]
+    case model
+      when "limit"
+        limit = Limit.find_by_id(params[:id])
+        limit.destroy unless limit.nil?
+      when "tariff"
+        tariff = Tariff.find_by_id(params[:id])
+        tariff.destroy unless tariff.nil?
+      when "package"
+        package = Package.find_by_id(params[:id])
+        package.destroy unless package.nil?
+    end
     render "tariff/index"
   end
 
