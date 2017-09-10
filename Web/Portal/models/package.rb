a59776a1928c9b6e338554ab3bf4c69f03584fae
Jan Mareš <jan.mares@manast.eu>
2017-09-11 00:30:06 +0200

    Oprava modelu Package pro mazání vazeb.

--- Web/Portal/models/package.rb
+++ Web/Portal/models/package.rb
@@ -1,7 +1,7 @@
 class Package < ActiveRecord::Base
   has_and_belongs_to_many :tariffs
   has_many :travels
-  
+
   has_many :mobiles, dependent: :delete_all
   has_many :sims, through: :mobiles
   has_many :personas, through: :mobiles
